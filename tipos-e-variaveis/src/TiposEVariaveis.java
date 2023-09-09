e6e9210b84deeb06cb7a1a292aaef712b4fdded6
rezzo <rezzoeduarda@gmail.com>
2023-09-09 12:21:29 -0300

    tornando o código mais abrangente

--- tipos-e-variaveis/src/TiposEVariaveis.java
+++ tipos-e-variaveis/src/TiposEVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposEVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Olá! Esse é meu primeiro código em Java");
     }
+    
+    //criação de uma classe
+    public class ClasseUm{
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        //o short em parenteses foi usado para converter uma variavel "grande" em "pequena"
+
+    }
+
 }
